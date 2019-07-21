891b3e954265fcba060b505b3c3f21d65621146d
Joshua Isaac <jayisaac0@gmail.com>
2019-07-22 02:41:46 +0300

    Update header.php

--- includes/widgets/header.php
+++ includes/widgets/header.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name = "viewport" content = "width = device-width, initial-scale = 1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />   
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link href="themes/css/mystyle.css" rel="stylesheet">
     <link href="themes/css/bootstrap.css" rel="stylesheet">
 	<script src="themes/jquery/jquery-3.0.0.min.js"></script>
