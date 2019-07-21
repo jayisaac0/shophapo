3eb471c09484c76aa5ff36165f20b3853ceeb112
Joshua Isaac <jayisaac0@gmail.com>
2019-07-22 02:39:31 +0300

    Update header.php

--- includes/widgets/header.php
+++ includes/widgets/header.php
@@ -10,8 +10,7 @@
     <link href="themes/css/bootstrap.css" rel="stylesheet">
 	<script src="themes/jquery/jquery-3.0.0.min.js"></script>
 	<script src="themes/js/bootstrap.min.js"></script>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+
 	<link rel="icon" href="images/logo.png">
 
     
